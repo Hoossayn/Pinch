04bc47fc250c891002de605c6a702b35e6dfa13c
hoossayn <abdulhakeemhusseni@gmail.com>
2022-11-25 23:12:08 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by hoossayn on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
